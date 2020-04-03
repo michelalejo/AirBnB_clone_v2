3aaea289bf13f47e6529c02d179616ff6c063b80
michelalejo <michelmolina65@gmail.com>
2020-04-03 04:52:23 +0000

    review,state,user

--- models/state.py
+++ models/state.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City', back_populates='state',
+            cascade='all, delete, delete-orphan')
+
+    @property
+    def cities(self):
+        cityes = []
+        for _id, city in models.storage.all(City).items():
+            if self.id == city.state_id:
+                cityes.append(city)
+        return cityes
