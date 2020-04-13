b8a73b3b48db41698ccdf6e165d980edf4558413
michelalejo <michelmolina65@gmail.com>
2020-04-13 03:02:19 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,20 +13,16 @@ def do_deploy(archive_path):
 
     try:
         files = archive_path.split("/")[-1]
-        route = "/data/web_static/releases"
-        put("{}".format(archive_path), "/tmp/{}".format(files))
-        directory = files.split(".")
-        run("mkdir -p {}/{}/".format(route, directory[0]))
-        new = '.'.join(directory)
-        run("tar -xzf /tmp/{} -C {}/{}/"
-            .format(new, route, directory[0]))
-        run("rm /tmp/{}".format(files))
-        run("mv {}/{}/web_static/* {}/{}/"
-            .format(route, directory[0], route, directory[0]))
-        run("rm -rf {}/{}/web_static".format(route, directory[0]))
-        run("rm -rf /data/web_static/current")
-        run("ln -sf {}/{} /data/web_static/current"
-            .format(route, directory[0]))
+        directory = files.split(".")[0]
+        route = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(route, directory))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(files, route, directory))
+        run('rm /tmp/{}'.format(files))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(route, directory))
+        run('rm -rf {}{}/web_static'.format(route, directory))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(route, directory))
         return True
     except:
         return False
