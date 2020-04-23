91cf413dc32cac6f2b76ddc45566051e5db6e133
michelalejo <michelmolina65@gmail.com>
2020-04-23 04:40:02 +0000

    9-states.py

---
+++ web_flask/9-states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# Script that starts a Flask web application.
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from operator import getitem
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """Display a HTML page with states_list."""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def tear_down_app(db):
+    """Method that remove the current SQLAlchemy Session."""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
