3a884367a2564a8d64ad03ca8b5d5410fed9d6b5
michelalejo <michelmolina65@gmail.com>
2020-04-23 04:33:05 +0000

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,9 @@
 # Script that starts a Flask web application.
 
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+from operator import getitem
 
 
 app = Flask(__name__)
