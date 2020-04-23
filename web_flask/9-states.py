550cf812884b11f57fbe3f398f9f9180ca36e017
michelalejo <michelmolina65@gmail.com>
2020-04-23 04:54:07 +0000

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -22,5 +22,12 @@ def tear_down_app(db):
     """Method that remove the current SQLAlchemy Session."""
     storage.close()
 
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_cities():
+    """Display a HTML page with cities_list."""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
